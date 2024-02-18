379baaab7363e2316e31cd00466cccee0015b401
Pamela <96056122+vitinha83627894832@users.noreply.github.com>
2024-02-18 12:18:45 -0300

    Delete adm directory

--- adm/mensagem.php
+++
@@ -1,7 +0,0 @@
-<?php if (isset($mensagem)) { ?>
-
-<div class="alert alert-success mt-5" role="alert">
-    <i class="fa-solid fa-square-check"></i>
-    <?= $mensagem // <?= é abreviação de echo?> 
-</div>
-<?php } ?> 
